8ac7020a0fbff2e2eeba58016f30663296ea7ebf
Reza Hedieloo <reza_hedieloo@yahoo.com>
2023-05-09 15:43:31 -0700

    Update files

--- server.js
+++ server.js
@@ -74,6 +74,10 @@ app.get('/search', (req, res) => {
     res.render('search');
 });
 
+app.get('/search'), (req, res) =>{
+    res.render('search');
+}
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
